25752e695fc8a340e7f3012df31025e25d302fc1
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-11-02 15:35:48 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+
+export default class App extends Component{
+  render(){
+    return(<h1 className="bg-red-700 text-white">Hello world</h1>)
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'montserrat': "font-family: 'Montserrat', sans-serif"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
