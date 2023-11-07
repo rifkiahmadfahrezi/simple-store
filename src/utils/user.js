9cbe59d2ea1df02c8cef110d55b93daa51053941
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-11-08 06:17:57 +0700

    create user login information data

---
+++ src/utils/user.js
@@ -0,0 +1,16 @@
+const userData = [
+	{
+		id: 1,
+		username: 'user',
+		password: 'user12345', // min 8 characters
+		type: 'buyer' // type = seller  or buyer
+	},
+	{
+		id: 2,
+		username: 'seller',
+		password: 'seller12345', // min 8 characters
+		type: 'seller' // type = seller  or buyer
+	}
+]
+
+export default userData
