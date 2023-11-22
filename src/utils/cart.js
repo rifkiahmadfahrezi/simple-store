3d4d89e42950cf92f9e0df8e512c555a77a103e6
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-11-22 21:50:59 +0700

    adding increase and decrease cart item

--- src/utils/cart.js
+++ src/utils/cart.js
@@ -9,28 +9,58 @@ const cart = {
           id: productInfo.id,
           title: productInfo.title,
           img: productInfo.thumbnail,
-          price: productInfo.price
+          price: productInfo.price,
+          stock: productInfo.stock
         }
 
         const cartProduct = this.items.find(item => item.id === cartItem.id) ?? false
     
         if(!cartProduct){
         	this.items.push({...cartItem, quantity: 1, totalProductPrice: cartItem.price})
-        	this.quantity++
         	this.totalPrice += cartItem.price
         }else{
         	this.items = this.items.map(item => {
         		if(item.id != cartItem.id) return item
-
-    			item.quantity++
+        		item.quantity++
     			item.totalProductPrice = item.price * item.quantity
 				this.totalPrice += item.price
 				return item
   
         	})
         }
+        localStorage.setItem('cart', JSON.stringify(this))
+	},
+	decrease: function(productInfo){
 
-		console.log("items:",this.items)
+		const cartProduct = this.items.find(item => item.id === productInfo.id) ?? false
+
+
+		if(cartProduct !== undefined){
+			this.items = this.items.map(item => {
+        		if(item.id != productInfo.id) return item
+
+        		item.quantity--
+    			item.totalProductPrice = item.price * item.quantity
+				this.totalPrice -= item.price
+				return item
+        	})
+		}else{
+			console.log('ok')
+		}
+
+		localStorage.setItem('cart', JSON.stringify(this))
+
+	},
+	remove: function(id){
+		this.items = this.items.filter(item => item.id !== id)
+		this.items.map(item => {
+			if(item.id === id){
+				this.totalPrice -= item.totalProductPrice
+			}
+		})
+
+		localStorage.setItem('cart', JSON.stringify(this))
+
 	}
 }
 
