3997e901d6d49bedd379fa6cb41a681503d5da79
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-11-28 06:28:42 +0700

    create account setting modal

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Link from './../elements/link/Link'
+import Input from './../elements/input/Input'
+import Label from './../elements/input/Label'
 import Searchbox from './searchbox/Searchbox'
 import Dropdown from '../Dropdown'
 import {getUser} from '../../utils/user.js'
@@ -13,16 +15,20 @@ import enUsConverter from 'number-to-text/converters/en-us'
 export default function Navbar({cartItems = {items: [], totalPrice: 0}, onSubmitHandler, addItem, decreaseItem, removeItem}){
 
 	// json.parse for change string to boolean
-	const [ isLogin, setIsLogin] = useState(JSON.parse(sessionStorage.getItem('login')))
-	const [ userInfo, setUserInfo ] = useState([])
+	const [ isLogin, setIsLogin] = useState(JSON.parse(sessionStorage.getItem('login')) ?? false)
+	const [ userInfo, setUserInfo ] = useState({})
 	const userId = sessionStorage.getItem('userId')
-    const [ modalActive, setModalActive ] = useState(false)
+    const [ modal, setModal ] = useState({isActive: false, target: null})
 
 	useEffect(() => {
 		if (cartItems == null) cartItems = {items: [], totalPrice: 0}
 		setUserInfo(getUser(userId))
 	}, [])
 
+	useEffect(()=> {
+		console.log(isLogin)
+		console.log(userInfo)
+	},[userInfo])
 
 	function doLogout(e){
 		e.preventDefault()
@@ -30,8 +36,27 @@ export default function Navbar({cartItems = {items: [], totalPrice: 0}, onSubmit
 		sessionStorage.setItem('login', false)
 	}
 
-	function toggleModal(){
-		modalActive ? setModalActive(false) : setModalActive(true) 
+	function toggleModal(target){
+		modal.isActive 
+		? setModal({isActive: false, target: null}) 
+		: setModal({isActive: true, target: target}) 
+	}
+
+
+	function checkOutHandler(e){
+		e.preventDefault()
+
+		if(!isLogin){
+			Swal.fire({
+	          title: 'Checkout failed',
+	          text: 'Please login before checkout',
+	          timer: 2500,
+	          timerProgressBar: true,
+	        })
+		}else{
+			console.log('do checkout')
+		}
+
 	}
 
 
@@ -49,7 +74,7 @@ export default function Navbar({cartItems = {items: [], totalPrice: 0}, onSubmit
 
 					<div className="relative">
 						<button 
-							onClick={()=> toggleModal() } 
+							onClick={()=> toggleModal('cart') } 
 							type="button" 
 							className="hover:bg-slate-50 rounded-md py-1 px-2">
 							<i className='text-indigo-900 text-2xl bx bx-cart'></i>
@@ -70,110 +95,160 @@ export default function Navbar({cartItems = {items: [], totalPrice: 0}, onSubmit
 							style="border rounded-md py-1 px-2 border-indigo-900 hover:text-white transition duration-300 hover:shadow-md hover:bg-indigo-900 capitalize"
 							>login</Link>
 							: 
-							userInfo.map((user, index) => {
-								return (<Dropdown text="Settings" key={index}>
-											<div className="py-2 text-center">
-												Hello!, {user.username}
-											</div>
-											<div 
-												onClick={e=> doLogout(e)} 
-												className="py-2 text-center cursor-pointer bg-slate-50 hover:bg-slate-100">
-												<span>logout</span>
-											</div>
-										</Dropdown>)
-							})
+							<Dropdown text="Settings">
+								<div className="py-2 text-center">
+									Hello!, {userInfo.surName || 'user'}
+								</div>
+								<div className="py-2 text-center cursor-pointer hover:bg-slate-100" onClick={()=>toggleModal('account-setting')}>
+									Account settings
+								</div>
+								<div 
+									onClick={e=> doLogout(e)} 
+									className="py-2 text-center cursor-pointer bg-slate-50 hover:bg-slate-100">
+									<span>logout</span>
+								</div>
+							</Dropdown>
 						}
 					</div>
 				</div>
 			</div>
 		</nav>
 
-		{modalActive ? 
+		{(modal.isActive && modal.target === 'cart') &&
 			<Modal>
 				<Modal.Body closeModalHandler={toggleModal}>
-				{cartItems.items?.length > 0 ? 
-				// console.log(cart)
-				<Modal.Header>
-					<div className="flex items-center justify-between mr-5">
-						<div className="flex flex-col">
-							<p className="text-lg">
-								<span className="font-bold">Total</span>
-								: ${cartItems.totalPrice == null ? convertDollar(cart.renewTotalPrice(cartItems)) : convertDollar(cartItems.totalPrice)}
-								<span className="ml-2">({cartItems.items.length} items)</span>
-							</p>
-							<span className="text-md text-slate-500">({numberToText.convertToText(cartItems.totalPrice, {case: 'lowerCase'})})</span>
-						</div>
+					{cartItems.items?.length > 0 ? 
+					// console.log(cart)
+					<Modal.Header>
+						<div className="flex items-center justify-between mr-5">
+							<div className="flex flex-col">
+								<p className="text-lg">
+									<span className="font-bold">Total</span>
+									: ${cartItems.totalPrice == null ? convertDollar(cart.renewTotalPrice(cartItems)) : convertDollar(cartItems.totalPrice)}
+									<span className="ml-2">({cartItems.items.length} items)</span>
+								</p>
+								<span className="text-md text-slate-500">({numberToText.convertToText(cartItems.totalPrice, {case: 'lowerCase'})})</span>
+							</div>
 
-						<Link 
-							to="/checkout" 
-							style="border rounded-md text-white py-1 px-2 transition duration-300 bg-indigo-900 capitalize"
-							>Check out</Link>
-					</div>
-				</Modal.Header>
-				: null
-				}
+							<button 
+								type="button" 
+								className="border rounded-md text-white py-1 px-2 transition duration-300 bg-indigo-900 capitalize"
+								onClick={(e)=>checkOutHandler(e)}
+								>Check out</button>
+						</div>
+					</Modal.Header>
+					: null
+					}
 
 					<div className="p-3">
 						{cartItems.items?.length > 0 ? 
-						cartItems.items.map((item, index) => {
-							return (
-								<div key={`${item.id}-${index}`} className="flex gap-3 border-b-2 border-indigo-900 py-2 mb-2 items-center justify-between">
-									<div className="flex">
-										<figure>
-											<img 
-											src={item.img} 
-											alt="image of products"
-											className="object-contain w-[100px] h-[100px]"/>
-										</figure>
-										<figcaption className="flex flex-col ml-2 justify-between">
-											<div>
-												<h4 className="font-semibold">{item.title}</h4>
-												<h6 className="font-reguler">
-													${item.price} &times; {item.quantity} = ${item.totalProductPrice}</h6>
-											</div>
+							cartItems.items.map((item, index) => {
+								return (
+									<div key={`${item.id}-${index}`} className="flex gap-3 border-b-2 border-indigo-900 py-2 mb-2 items-center justify-between">
+										<div className="flex">
+											<figure>
+												<img 
+												src={item.img} 
+												alt="image of products"
+												className="object-contain w-[100px] h-[100px]"/>
+											</figure>
+											<figcaption className="flex flex-col ml-2 justify-between">
+												<div>
+													<h4 className="font-semibold">{item.title}</h4>
+													<h6 className="font-reguler">
+														${item.price} &times; {item.quantity} = ${item.totalProductPrice}</h6>
+												</div>
 
-											<form onSubmit={e=> e.preventDefault()} className="flex rounded-sm">
-												<button 
-													type="button"
-													className="bg-indigo-50 py-1 px-2"
-													onClick={()=> decreaseItem(item)} >
-													<i className='bx bx-minus'></i>
-												</button>
-												<input 
-													onChange={(e)=> setQuantity(e.target.value)} 
-													onInput={(e)=> setQuantity(e.target.value)} 
-													className="w-[60px] text-center bg-slate-50" 
-													type="number"
-													value={ item.quantity }/>
-												<button 
-													type="button"
-													className="bg-indigo-50 py-1 px-2"
-													onClick={()=> addItem(item)}>
-													<i className='bx bx-plus'></i>
-												</button>
-											</form>
-										</figcaption>
+												<form onSubmit={e=> e.preventDefault()} className="flex rounded-sm">
+													<button 
+														type="button"
+														className="bg-indigo-50 py-1 px-2"
+														onClick={()=> decreaseItem(item)} >
+														<i className='bx bx-minus'></i>
+													</button>
+													<input 
+														onChange={(e)=> setQuantity(e.target.value)} 
+														onInput={(e)=> setQuantity(e.target.value)} 
+														className="w-[60px] text-center bg-slate-50" 
+														type="number"
+														value={ item.quantity }/>
+													<button 
+														type="button"
+														className="bg-indigo-50 py-1 px-2"
+														onClick={()=> addItem(item)}>
+														<i className='bx bx-plus'></i>
+													</button>
+												</form>
+											</figcaption>
+										</div>
+										<div>
+											<button 
+												type="button" 
+												className="text-red-500 p-2 rounded-sm text-2xl hover:bg-red-50"
+												onClick={()=> {
+													removeItem(item.id)
+												}}>
+												<i className='bx bx-trash'></i>
+											</button>
+										</div>
 									</div>
-									<div>
-										<button 
-											type="button" 
-											className="text-red-500 p-2 rounded-sm text-2xl hover:bg-red-50"
-											onClick={()=> {
-												removeItem(item.id)
-											}}>
-											<i className='bx bx-trash'></i>
-										</button>
-									</div>
+								)
+							}).sort().reverse()
+							: <p className="text-slate-500 text-center">Cart is empty :(</p>
+						}
+						</div>
+
+					</Modal.Body>
+			</Modal>
+		}
+
+		{(modal.isActive && modal.target === 'account-setting') &&
+			<Modal>
+				<Modal.Body closeModalHandler={toggleModal}>
+					<div className="p-3 flex flex-col gap-3 mb-8">
+						<form action="">
+							<div className="flex flex-col">
+								<Label style="mb-2 capitalize">name:</Label>
+								<Input value={userInfo.surName} style="bg-slate-50 rounded-md"/>
+							</div>
+							<div className="flex flex-col">
+								<Label style="mb-2 capitalize">username:</Label>
+								<Input value={userInfo.username} style="bg-slate-50 rounded-md"/>
+							</div>
+							<div className="flex flex-col">
+								<Label style="mb-2 capitalize">e-mail:</Label>
+								<Input value={userInfo.email} style="bg-slate-50 rounded-md"/>
+							</div>
+							<div className="flex flex-col">
+								<Label style="mb-2 capitalize">full address:</Label>
+								<Input value={userInfo.address.fullAdress} style="bg-slate-50 rounded-md"/>
+							</div>
+							<div className="grid grid-cols-3 gap-1">
+								<div className="flex flex-col">
+									<Label style="mb-2 capitalize">country:</Label>
+									<Input value={userInfo.address.country} style="bg-slate-50 rounded-md"/>
 								</div>
-							)
-						}).sort().reverse()
-						: <p className="text-slate-500 text-center">Cart is empty :(</p>
-					}
-					</div>
+								<div className="flex flex-col">
+									<Label style="mb-2 capitalize">state:</Label>
+									<Input value={userInfo.address.state} style="bg-slate-50 rounded-md"/>
+								</div>
+								<div className="flex flex-col">
+									<Label style="mb-2 capitalize">city:</Label>
+									<Input value={userInfo.address.city} style="bg-slate-50 rounded-md"/>
+								</div>
+							</div>
 
+							<Modal.Header>
+								<button 
+									className="font-montserrat py-2 px-3 rounded-md bg-indigo-900 mt-3 text-white" 
+									type="submit">
+									Update</button>
+							</Modal.Header>
+
+						</form>
+					</div>
 				</Modal.Body>
 			</Modal>
-		: null
 		}
 
 	</>
