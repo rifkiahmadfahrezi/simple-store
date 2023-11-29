c8e15848d79d866020de2ac236fa099125a102a6
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-11-29 08:32:05 +0700

    update code

--- src/component/modal/index.jsx
+++ src/component/modal/index.jsx
@@ -11,18 +11,20 @@ export default function Modal({children}){
 
 export function Body({closeModalHandler, children}){
 	return (
-		<div className="bg-red-500 z-[-1] w-[90%] h-[90%] md:max-w-2xl rounded-md shadow-md max-h-[500px] relative">
+		<>
+		<div className="bg-white h-[100%] w-[90%] md:max-w-2xl opacity-1 rounded-md shadow-md max-h-[500px] relative">
 			<Modal.CloseModal closeModalHandler={closeModalHandler}/>
-			<div className="flex rounded-md flex-col bg-white max-h-[420px] py-5 mb-[200px] overflow-y-auto">
+			<div className=" flex flex-col bg-white max-h-[408px] py-5  overflow-y-auto">
 				{children}
 			</div>
 		</div>
+		</>
 	)
 }
 
 export function Footer({children}){
 	return (
-		<div className="flex flex-col px-2 absolute bottom-0 left-0 bg-slate-50 w-full rounded-md py-5">
+		<div className="flex flex-col px-2 absolute left-0 bottom-0 bg-white w-full rounded-md py-5">
 			{children}
 		</div>
 	)
