6587490909b7e85fd4320a769ad382454201749a
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-12-01 06:13:46 +0700

    add discountedPrice function

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,3 +1,9 @@
 export function convertDollar(number = 0){
 	return new Intl.NumberFormat("en-US").format(number)
 }
+
+export function getDiscountedPrice(discount = 0, actualPrice = 0){
+    const discountPrice = Number(actualPrice) * Number(discount) / 100
+    const discountedPrice = actualPrice - discountPrice 
+    return Math.round(discountedPrice)
+}
