5d2a74e571a9c730f5d6d4fcf4d0890c695eaa0e
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-12-04 06:13:44 +0700

    add props function

--- src/component/pagination/index.jsx
+++ src/component/pagination/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-export default function pagination({dataLength = 1}){
+export default function pagination({activePage = 1, dataLength = 1, perPage = 20, numberPageClickHandler, prevPageClickHandler, nextPageClickHandler}){
 
 	const pageNumber = []
-	for(let i = 1; i <= Number(dataLength) / 20; i++){
+	for(let i = 1; i <= Math.round(Number(dataLength) / Number(perPage)); i++){
 		pageNumber.push(<li key={i}>
-					      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight border-e-0 text-indigo-900 bg-white border border-indigo-900 hover:bg-indigo-100 hover:text-indigo-700">{i}</a>
+					      <button 
+					      	type="button" 
+					      	className="flex items-center justify-center px-4 h-10 leading-tight border-e-0 text-indigo-900 bg-white border border-indigo-900 hover:bg-indigo-100 hover:text-indigo-700"
+					      	onClick={(e)=> numberPageClickHandler(e)}
+					      	>{i}</button>
 					    </li>)
 	}
 
@@ -16,7 +20,11 @@ export default function pagination({dataLength = 1}){
 		    {
 		    	dataLength > 1 && 
 			    <li>
-			      <a href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-indigo-900 bg-white border border-e-0 border-indigo-900 rounded-s-lg hover:bg-indigo-100 hover:text-indigo-700">Previous</a>
+			      <button 
+			      	type="button" 
+			      	className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-indigo-900 bg-white border border-e-0 border-indigo-900 rounded-s-lg hover:bg-indigo-100 hover:text-indigo-700"
+			      	onClick={(e)=> prevPageClickHandler(e)}
+			      	>Previous</button>
 			    </li>
 			}
 
@@ -27,7 +35,11 @@ export default function pagination({dataLength = 1}){
 		   {
 		    	dataLength > 1 && 
 			    <li>
-			      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-indigo-900 bg-white border border-indigo-900 rounded-e-lg hover:bg-indigo-100 hover:text-indigo-700">Next</a>
+			      <button
+			      	type="button" 
+			      	className="flex items-center justify-center px-4 h-10 leading-tight text-indigo-900 bg-white border border-indigo-900 rounded-e-lg hover:bg-indigo-100 hover:text-indigo-700"
+			      	onClick={(e)=> nextPageClickHandler(e)}
+			      	>Next</button>
 			    </li>
 			}
 		  </ul>
