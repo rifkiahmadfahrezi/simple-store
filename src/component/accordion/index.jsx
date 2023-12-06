c660eb141decb4c8abe626eba913526dc0f062db
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-12-06 16:32:53 +0700

    change bg color

--- src/component/accordion/index.jsx
+++ src/component/accordion/index.jsx
@@ -15,7 +15,7 @@ export function Trigger({children, target, toggleAccordion, isOpen}){
 
 	return(
 		<>
-			<h2 id={target} onClick={(e) => toggleAccordion(e)} className={`z-[99] ${isOpen ? 'bg-indigo-100' : 'bg-white'}`}>
+			<h2 id={target} onClick={(e) => toggleAccordion(e)} className={`transition-all duration-300 ${isOpen ? 'bg-indigo-50 hover:bg-indigo-200' : 'bg-white hover:bg-indigo-50'}`}>
 			    <button id={target} type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-indigo-900" aria-expanded="true" aria-controls="accordion-collapse-body-1">
 			      {children}
 			      <i id={target} className={`bx text-2xl transition duration-300 bx-chevron-down ${isOpen ? 'rotate-180' : 'rotate-0'}`}></i>
@@ -31,7 +31,7 @@ export function Content({isOpen, children, style}){
 	return(
 <>
 	<div
-		className={`transition-all  duration-300 h-full ${isOpen ? 'h-full' : 'h-0'} ${style}`} 
+		className={`transition-all  duration-300 ${isOpen ? 'h-full' : 'h-0'} ${style}`} 
 		aria-labelledby="accordion-collapse">
 	    <div className="p-5">
 	      {children}
