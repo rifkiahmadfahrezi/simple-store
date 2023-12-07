7e9d96f4ec52d27635548c11b3bc05a928d97a37
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-12-07 15:21:06 +0700

    update css

--- src/component/pagination/index.jsx
+++ src/component/pagination/index.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 export default function pagination({activePage = 1, dataLength = 1, perPage = 20, numberPageClickHandler, prevPageClickHandler, nextPageClickHandler}){
 
+	const numOfPage = Math.round(Number(dataLength) / Number(perPage))
+
 	const pageNumber = []
-	for(let i = 1; i <= Math.round(Number(dataLength) / Number(perPage)); i++){
+	for(let i = 1; i <= numOfPage ; i++){
 		pageNumber.push(<li key={i}>
 					      <button 
-					      	type="button" 
-					      	className="flex items-center justify-center px-4 h-10 leading-tight border-e-0 text-indigo-900 bg-white border border-indigo-900 hover:bg-indigo-100 hover:text-indigo-700"
+					      	type="button"
+					      	data-activepage={i}
+					      	className={`
+					      		${activePage == i ? 'bg-indigo-100' : 'bg-white'} 
+					      		${numOfPage <= 1 ? 'rounded-md' : 'border-e-0'}
+					      		flex items-center justify-center px-4 h-10 leading-tight  text-indigo-900 border border-indigo-900 hover:bg-indigo-100 hover:text-indigo-700`}
 					      	onClick={(e)=> numberPageClickHandler(e)}
 					      	>{i}</button>
 					    </li>)
@@ -18,7 +24,7 @@ export default function pagination({activePage = 1, dataLength = 1, perPage = 20
 		<nav aria-label="Page navigation" className="container mt-8 w-full mx-auto">
 		  <ul className="flex items-center justify-center">
 		    {
-		    	dataLength > 1 && 
+		    	(numOfPage > 1 && activePage > 1 )?
 			    <li>
 			      <button 
 			      	type="button" 
@@ -26,6 +32,7 @@ export default function pagination({activePage = 1, dataLength = 1, perPage = 20
 			      	onClick={(e)=> prevPageClickHandler(e)}
 			      	>Previous</button>
 			    </li>
+			    : null
 			}
 
 
@@ -33,14 +40,16 @@ export default function pagination({activePage = 1, dataLength = 1, perPage = 20
 
 
 		   {
-		    	dataLength > 1 && 
+		    	(numOfPage > 1 )?
 			    <li>
 			      <button
+			      	disabled={activePage < numOfPage ? false : true}
 			      	type="button" 
-			      	className="flex items-center justify-center px-4 h-10 leading-tight text-indigo-900 bg-white border border-indigo-900 rounded-e-lg hover:bg-indigo-100 hover:text-indigo-700"
+			      	className={`flex items-center justify-center px-4 h-10 leading-tight text-indigo-900 bg-white border border-indigo-900 rounded-e-lg hover:bg-indigo-100 hover:text-indigo-700 ${activePage == numOfPage ? 'cursor-not-allowed bg-slate-200' : 'cursor-pointer'}`}
 			      	onClick={(e)=> nextPageClickHandler(e)}
 			      	>Next</button>
 			    </li>
+			    :null
 			}
 		  </ul>
 		</nav>
